1fe26d45764a353dea9196270f75037b9e5ea4e3
Kimheang-Developer <simkimheang4@gmail.com>
2022-09-14 16:56:13 +0700

    first commit

--- resources/js/layouts/Sidebar.jsx
+++ resources/js/layouts/Sidebar.jsx
@@ -1,34 +1,26 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import { menus } from '../local-json/data'
-import { BiHomeAlt } from "react-icons/bi";
 
 function Sidebar() {
-
-    const style = {
-        logo: '',
-        left_sidebar_container: '',
-        side_nav: 'list-none pl-0',
-        side_nav_item: '',
-        side_nav_link: '',
-        side_nav_text: 'align-middle ml-[10px]'
-    }
-
     return (
         <>
             <aside className="left_sidebar">
                 <Link to="" className="logo">
 
                 </Link>
-                <div className={style.left_sidebar_container}>
-                    <ul className={style.side_nav}>
-                        {menus && menus.map(({ id, name, icon }) => (
-                            <li className={style.side_nav_item} key={id}>
-                                <Link to="" className="block text-[.9375rem] text-[#8391a2] py-[10px] px-[30px]">
-                                    <span className={style.side_nav_text}>{name}</span>
-                                </Link>
-                            </li>
-                        ))}
+                <div className="left_sidebar_container">
+                    <ul className="side_nav">
+                        {menus.map(( menu, index) => {
+                            return (
+                                <li className="side_nav_item" key={menu.id}>
+                                    <Link to={menu.url} className="side_nav_link">
+                                        <menu.icon/>
+                                        <span className="side_nav_text">{menu.name}</span>
+                                    </Link>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
             </aside>
--- resources/js/local-json/data.js
+++ resources/js/local-json/data.js
@@ -1,10 +1,11 @@
 import { MdAccountCircle, MdSettings, MdSupport, MdHelpOutline, MdLogout } from "react-icons/md";
+import { AiOutlineDashboard, AiOutlineUser, AiOutlineSetting } from "react-icons/ai";
 
 export const menus = [
-    { id: 1, name: 'Dashboard', icon: 'BiHomeAlt' },
-    { id: 2, name: 'Users', icon: 'BiHomeAlt' },
-    { id: 3, name: 'Settings', icon: 'BiHomeAlt' },
-    { id: 4, name: 'Logout', icon: 'BiHomeAlt' }
+    { id: 1, name: 'Dashboard', icon: AiOutlineDashboard, url: '' },
+    { id: 2, name: 'Users', icon: AiOutlineUser, url: '' },
+    { id: 3, name: 'Settings', icon: AiOutlineSetting, url: '' },
+    { id: 4, name: 'Logout', icon: MdLogout, url: '' }
 ]
 
 export const profilemenus = [
